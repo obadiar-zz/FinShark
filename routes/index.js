4a4b56c07ceaea95681e675a0b8821fa66bd0059
jeff <tangj1122@gmail.com>
2017-10-07 13:18:14 -0700

    set route for /upload

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'Express' });
 });
 
+router.get('/upload', function(req, res, next) {
+  res.render('upload');
+});
 module.exports = router;
