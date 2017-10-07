24adaf8a2e8656196d3a9a1b8f290649f3324c54
obadiar <ob.diar@gmail.com>
2017-10-07 15:40:32 -0700

    Test app

--- test.js
+++ test.js
@@ -1,4 +1,12 @@
 var PDFtoText = require('./Watson/PDFtoText')
+var languageTranslator = require('./Watson/Translation')
+var TexttoSpeech = require('./Watson/TextToSpeech')
 
-PDFtoText('pdf-sample', console.log)
+PDFtoText('pdf-sample', (response)=>{
+    languageTranslator(response, 'en', 'es', (response) => {
+        TexttoSpeech(response, 'es-US_SofiaVoice', (response) => {
+            console.log('PDF is now in voice format!')
+        })
+    })
+})
 
