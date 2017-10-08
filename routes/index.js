90fbc87d2ff26bd1759270bcf0556593d7712538
jeff <tangj1122@gmail.com>
2017-10-08 02:18:49 -0700

    persistence now works for lang translation

--- routes/index.js
+++ routes/index.js
@@ -11,59 +11,62 @@ router.get('/', function(req, res, next) {
     upload: "Upload",
   };
 
-  if (lang === 'en') {
-    res.render('home', { title: 'FinShark', text: text });
-  }
-  if (lang) {
-    async.forEachOf(text, (val, key, cb) => {
-      try {
-        languageTranslator(key, 'en', lang, (translation) => {
-          text[key] = translation;
-          cb();
-        });
-      }
-      catch (e) {
-        return cb(e);
-      }
-    }, (err) => {
-      if (err) console.log(err);
-      res.json({"text": text});
-    })
-
-  }
+  lang === 'en' 
+    ? res.render('home', { title: 'FinShark', text: text })
+    : objTranslator(text, lang, res, 'json');
 });
 
 router.post('/', (req, res) => {
-  /* var lang = req.body.lang; */
-  /* console.log(lang) */
   res.redirect(`/?lang=${req.body.lang}`);
 });
 
 router.get('/upload', function(req, res, next) {
-  res.render('upload');
+  const lang = req.query.lang || 'en';
+  let text = {
+    h1: "Upload Document",
+    label: "Choose a file or drag it here.",
+    submit: "Submit",
+  };
+
+  lang === 'en' 
+    ? res.render('upload', {text: text})
+    : objTranslator(text, lang, res, 'render', 'upload');
+  
 });
 
-const str1 = "Lorem ipsum dolor sit amet consectetur adipiscing, elit iaculis quisque ligula dapibus taciti, luctus aliquet maecenas nibh sociis. Iaculis sagittis commodo feugiat porttitor magna praesent eros, ullamcorper ac aenean aptent eget viverra convallis"
-const str2 = "Massa tellus bibendum vulputate eros quam aliquet fermentum dapibus leo auctor"
-const str3 = "Fames etiam primis curabitur tempor convallis habitasse litora enim, lacus tincidunt ante"
-
-const exampleData = [
-  {msgType: 'Good News', msg: str1, color: 'lightgreen'},
-  {msgType: 'Bad News', msg: str2, color: 'red'},
-  {msgType: 'Next Steps', msg: str3, color: ''}
-]
-
 router.get('/doc', function(req, res, next) {
-  /* let tmp = null; */
-  /* languageTranslator('Hello, my name is Rob.', 'en', 'es', (x) => { */
-  /*   console.log('x', x); */
-  /*   tmp = x; */
-  /*   console.log('tmp', tmp) */
-  /* }) */
 
-   // when we get real data, we need to render doc with translated data
+  const str1 = "Lorem ipsum dolor sit amet consectetur adipiscing, elit iaculis quisque ligula dapibus taciti, luctus aliquet maecenas nibh sociis. Iaculis sagittis commodo feugiat porttitor magna praesent eros, ullamcorper ac aenean aptent eget viverra convallis"
+  const str2 = "Massa tellus bibendum vulputate eros quam aliquet fermentum dapibus leo auctor"
+  const str3 = "Fames etiam primis curabitur tempor convallis habitasse litora enim, lacus tincidunt ante"
+  const exampleData = [
+    {msgType: 'Good News', msg: str1, color: 'lightgreen'},
+    {msgType: 'Bad News', msg: str2, color: 'red'},
+    {msgType: 'Next Steps', msg: str3, color: ''}
+  ]
+
   res.render('doc', {data: exampleData});
 
 });
+
+function objTranslator(text, lang, res, resType, view) {
+  async.forEachOf(text, (val, key, cb) => {
+    try {
+      languageTranslator(val, 'en', lang, (translation) => {
+        console.log('\nval', val, '\nkey', key, '\ntranslation', translation)
+        text[key] = translation;
+        cb();
+      });
+    }
+    catch (e) {
+      return cb(e);
+    }
+  }, (err) => {
+    if (err) console.log(err);
+    if (resType === 'json') res.json({"text": text});
+    console.log('translated',text)
+    if (resType === 'render') res.render(view, {"text": text});
+  })
+}
 
 module.exports = router;
