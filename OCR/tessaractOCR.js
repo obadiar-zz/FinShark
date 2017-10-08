e895c5edd7c9583a52bec691438c7680d1cc7284
robertdurst <rsdurst@colby.edu>
2017-10-07 19:00:06 -0700

    OCR text extraction algorithm

--- OCR/tessaractOCR.js
+++ OCR/tessaractOCR.js
@@ -9,48 +9,13 @@ Only input is an image as specified below.
     3) a ImageData instance (an object containing width, height and data properties)
 */
 
-detectText = (img, callback) => {
+detectText = (img, keywords, dataAnalysis, callback) => {
   Tesseract.recognize(img, { lang: "eng" })
     .catch(err => console.error(err))
     .then(result => {
       var text = result.text;
-      callback(text);
+      dataAnalysis(text, keywords, callback);
     });
 }
-
-// OCR test
-// Specify an image path and then call detectText with that and a callback
-/*
-  Example
-    Input:
-    var testImage = __dirname+"/testocr.png"
-    detectText(testImage, console.log);
-
-
-    Output:
-
-    "Aobe Acrobat PDF Files
-
-    Adobe PDF is an ideal formal for eleerronie doeurnenr distribution as il overcomes the
-
-    problems commonly eneounrered wills eleerrenie ﬁle sharing.
-
-    . Anyone. anywhere can open a PDF ﬁles All you need is use free Adnbe Aerobar
-    lmdzr. Reclplenls of nLher ﬁle far-mars somenrnes can‘t open fries amuse rbey
-    dlyn‘l have Lhe appliearrans used In mm rlae daeurn-rs.
-
-    . PDF ﬁles always printmrrccdy on any priming device.
-
-    . PDF ﬁles always display merry as creamd. regardless of ions, sonware, and
-    aperaring syslcms. Fonts, and graphics are nor 105! due la plarron-n saﬁwrre, and
-    version incumpatihililics.
-
-    . The free Acrobat Reader is easy to download and can be freely distributed by
-    anyone,
-
-    - Compaer PDF ﬁles are smaller than rbair souree ﬂies and download a
-    page ar a rime for Lasr display an lhe Web.""
-
-*/
 
 module.exports = detectText;
